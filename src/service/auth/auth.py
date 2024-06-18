44246f0ce65bb52136bacf2e190918ea5b7e72a0
loyal812 <jh.chan0812@gmail.com>
2024-06-17 21:54:04 -0400

    feat: complete sign in & up basic structure

--- src/service/auth/auth.py
+++ src/service/auth/auth.py
@@ -1,13 +1,3 @@
-import os
-import secrets
-from dotenv import load_dotenv
-from src.core.db.mongodb.cl_mongodb import MongoDB
-
-from datetime import datetime
-
-load_dotenv()
-mongodb_collection_name = os.getenv('MONGODB_COLLECTION_NAME')
-
 def perform_signup(email: str, password: str, password2: str):
     return
 
